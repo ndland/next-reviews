93ff1ec62d13edd412a7c1868c2597cc60816790
Nick Land <land.d.nicholas@gmail.com>
2024-02-24 19:00:59 -0500

    feat(dynamicroutes): refactor our reviews to use dynamic routes via Next.js

---
+++ app/reviews/[slug]/page.jsx
@@ -0,0 +1,24 @@
+import Heading from "@/components/Heading";
+import { getReview } from "@/lib/reviews";
+
+export default async function ReviewPage({ params: { slug } }) {
+  const { title, date, image, body } = await getReview(slug);
+
+  return (
+    <>
+      <Heading>{title}</Heading>
+      <p className="italic pb-2">{date}</p>
+      <img
+        src={image}
+        alt=""
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
+      <article
+        dangerouslySetInnerHTML={{ __html: body }}
+        className="max-w-screen-sm prose prose-slate"
+      />
+    </>
+  );
+}

61384550ae6c78ca928d3da5c0f825eda066aa6f
Nick Land <land.d.nicholas@gmail.com>
2024-02-26 12:20:31 -0500

    feat(metadata): add metadata to site
    * Add favicon
    * This concludes the Metadata portion of the course.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Heading from '@/components/Heading'
+import React from 'react';
+import Heading from '@/components/Heading';
+
+export const metadata = {
+  title: 'About',
+};
 
 export default function About() {
   return (
     <>
       <Heading>About</Heading>
-      <p>
-        A site to learn Next.js.
-      </p>
+      <p>A site to learn Next.js.</p>
     </>
-  )
+  );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,19 +2,30 @@ import { RobotoSlab, WorkSans } from './fonts';
 import './globals.css';
 import NavBar from '@/components/NavBar';
 
+export const metadata = {
+  title: {
+    default: 'Indie Gamer',
+    template: '%s | Indie Gamer',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${RobotoSlab.variable} ${WorkSans.variable}`}>
-      <body className='flex flex-col bg-orange-50 px-4 py-2 min-h-screen'>
+      <body className="flex flex-col bg-orange-50 px-4 py-2 min-h-screen">
         <header>
           <NavBar />
         </header>
-        <main className='grow py-3'>
-          {children}
-        </main>
-        <footer className='border-t py-3 text-center text-slate-500 text-xs'>
-          Game data and images courtesy
-          of <a href="https://rawg.io" target='_blank' className="text-orange-800 hover:underline">RAWG</a>
+        <main className="grow py-3">{children}</main>
+        <footer className="border-t py-3 text-center text-slate-500 text-xs">
+          Game data and images courtesy of{' '}
+          <a
+            href="https://rawg.io"
+            target="_blank"
+            className="text-orange-800 hover:underline"
+          >
+            RAWG
+          </a>
         </footer>
       </body>
     </html>

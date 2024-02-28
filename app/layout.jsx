5bb7b78af3d0dbc9f50068aa01c695f426f0fdc8
Nick Land <land.d.nicholas@gmail.com>
2024-02-28 10:12:09 -0500

    Deployed on Vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,12 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${RobotoSlab.variable} ${WorkSans.variable}`}>
-      <body className="flex flex-col bg-orange-50 px-4 py-2 min-h-screen">
+      <body className="flex min-h-screen flex-col bg-orange-50 px-4 py-2">
         <header>
           <NavBar />
         </header>
         <main className="grow py-3">{children}</main>
-        <footer className="border-t py-3 text-center text-slate-500 text-xs">
+        <footer className="border-t py-3 text-center text-xs text-slate-500">
           Game data and images courtesy of{' '}
           <a
             href="https://rawg.io"
@@ -25,7 +25,8 @@ export default function RootLayout({ children }) {
             className="text-orange-800 hover:underline"
           >
             RAWG
-          </a>
+          </a>{' '}
+          Deployed on Vercel.
         </footer>
       </body>
     </html>
